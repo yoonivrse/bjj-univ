9ca45ab6d9dd4c75b83983ca550a9968e53bdfb3
univrse <choys@sogang.ac.kr>
2026-06-28 22:39:26 +0900

    metadata에 openGraph 추가, keywords 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,24 +17,28 @@ const geistMono = Geist_Mono({
 
 
 export const metadata: Metadata = {
-  title: "전국 대학생 주짓수 대회",
-  description: "T.A.P 주최 전국 대학생 주짓수 대회 공식 홈페이지입니다.",
-  keywords: "전국대학생주짓수대회, 전대주, TAP, tap, 이대, 서강대, 주짓수 대회, 대학생 주짓수 대회",
-  robots: "index, follow",
+  title: "전대주 | 전국 대학생 주짓수 대회",
+  description: "서강대학교 x 이화여대 주짓수 동아리 T.A.P이 주최하는 전국 대학생 주짓수 대회 공식 홈페이지입니다.",
+  keywords: ["전대주", "전국대학생주짓수대회", "주짓수", "BJJ", "대학생 대회", "T.A.P", "서강대", "이화여대"],
+  openGraph: {
+    title: "전대주 | 전국 대학생 주짓수 대회",
+    description: "서강대학교 x 이화여대 주짓수 동아리 T.A.P이 주최하는 전국 대학생 주짓수 대회",
+    url: "https://bjj-univ-test.netlify.app",
+    siteName: "전대주",
+    locale: "ko_KR",
+    type: "website",
+  },
 };
-
-
-
+ 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
         <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
 }
-
-
+ 
